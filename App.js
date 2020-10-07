f69e3c42bf6de7f97665ed8e48c6237cbc9c949d
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-10-07 23:21:40 +0700

    Finish section A of milestone 3: display weather data

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import Constants from "expo-constants";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default class App extends React.Component{
   constructor(props){
@@ -43,10 +44,64 @@ export default class App extends React.Component{
         this.setState({error: true, loading: false})
       }
     })
-  }
+  };
   render(){
+    const WeatherCard = ({location, error, loading}) => {
+      const temperatureC = (location.main.temp-273.15).toFixed(2);
+      const temperatureF= (((location.main.temp -273.15)*9)/5+32).toFixed(2);
+      const description = location.weather[0].description;
+      const icon = location.weather[0].main;
+
+      const capitalizedDescription = description && description.charAt(0).toUpperCase() + description.slice(1);
+      if(error) {
+        return <View style={styles.container}>
+          <Text>Error fetching weather</Text>
+        </View>
+      };
+    }
     return(
-      <Text style={styles.container}>Hello</Text>
+      <View style={styles.container}>
+        <View style={styles.weatherContainer}>
+          {/* {loading && <Loading />} */}
+          <View style={styles.row}>
+            <MaterialIcons name="location-city" size={25} color="lightgrey" />
+            <Text style={styles.locationText}>{location.name}</Text>
+          </View>
+          <View style={[styles.row, { marginTop: 10 }]}>
+            <MaterialCommunityIcons
+              size={25}
+              color="lightgrey"
+              name="speedometer"
+            />
+            <Text style={styles.text}>{windSpeed}</Text>
+          </View>
+          <View style={styles.row}>
+            <MaterialCommunityIcons
+              size={25}
+              color="lightgrey"
+              name={getWeatherIcon(icon)}
+            />
+            <Text style={styles.text}>{capitalizedDescription}</Text>
+          </View>
+          <View style={styles.tempRow}>
+          <View style={styles.row}>
+            <MaterialCommunityIcons
+              size={25}
+              color="lightgrey"
+              name="temperature-fahrenheit"
+            />
+            <Text style={styles.text}>{temperatureF}</Text>
+          </View>
+          <View style={styles.row}>
+            <MaterialCommunityIcons
+              size={25}
+              color="lightgrey"
+              name="temperature-celsius"
+            />
+            <Text style={styles.text}>{temperatureC}</Text>
+          </View>
+        </View>
+      </View>
     );
   }
 }
